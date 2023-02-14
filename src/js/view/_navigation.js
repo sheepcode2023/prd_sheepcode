8a967f7114e9301cb3ca6554f1665630d0731b68
Yoichi Hashimoto <hashimoto@saunashi.com>
2023-02-14 10:52:02 +0900

    code modifiled

--- src/js/view/_navigation.js
+++ src/js/view/_navigation.js
@@ -1,5 +1,8 @@
 const spmenu_trigger = document.getElementById("spmenu_trigger");
-
-spmenu_trigger.addEventListener("click", () => {
-	spmenu_trigger.classList.toggle("is-active");
-});
+const sp_navigation = document.getElementById("sp_navigation");
+if (spmenu_trigger) {
+	spmenu_trigger.addEventListener("click", () => {
+		spmenu_trigger.classList.toggle("is-active");
+		sp_navigation.classList.toggle("is-active");
+	});
+}
